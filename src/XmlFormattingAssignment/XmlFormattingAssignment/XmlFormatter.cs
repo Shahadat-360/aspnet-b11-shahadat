c586687d7f313bc5f0cc148ead5266c5b7c736b6
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-12-16 21:30:18 +0600

    Assignment Done

--- src/XmlFormattingAssignment/XmlFormattingAssignment/XmlFormatter.cs
+++ src/XmlFormattingAssignment/XmlFormattingAssignment/XmlFormatter.cs
@@ -6,9 +6,56 @@ namespace XmlFormattingAssignment
 {
     public static class XmlFormatter
     {
+        //Reducing Repetition
+        private static void Tagger(StringBuilder stringBuilder, Object obj, string Tag)
+        {
+            stringBuilder.Append($"<{Tag}>");
+            if (obj != null)
+                stringBuilder.Append($"{obj.ToString()}");
+            stringBuilder.Append($"</{Tag}>");
+        }
+
+        //Solver(Logic,Control,Program Flow)
+        private static void XmlCreator(StringBuilder stringBuilder, Object obj, string Tag)
+        {
+            //For Null value
+            if (obj is null)
+            {
+                Tagger(stringBuilder, obj, Tag);
+                return;
+            }
+            var type = obj.GetType();
+            //For Properties
+            if (type.IsPrimitive || type == typeof(decimal) || type == typeof(string) || type == typeof(DateTime))
+            {
+                Tagger(stringBuilder, obj, Tag);
+            }
+            //For Collection
+            else if (obj is IEnumerable && type != typeof(string))
+            {
+                stringBuilder.Append($"<{Tag}>");
+                foreach (var item in (IEnumerable)obj)
+                {
+                    XmlCreator(stringBuilder, item, item.GetType().Name);
+                }
+                stringBuilder.Append($"</{Tag}>");
+            }
+            //For Class 
+            else
+            {
+                stringBuilder.Append($"<{Tag}>");
+                foreach (var property in type.GetProperties())
+                {
+                    XmlCreator(stringBuilder, property.GetValue(obj), property.Name);
+                }
+                stringBuilder.Append($"</{Tag}>");
+            }
+        }
         public static string Convert(object obj)
         {
-            throw new NotImplementedException();
+            StringBuilder stringBuillder = new StringBuilder();
+            XmlCreator(stringBuillder, obj, obj.GetType().Name);
+            return stringBuillder.ToString();
         }
     }
 }
