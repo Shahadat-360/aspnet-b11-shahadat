9735cb764363a7b7133a093819d33a85fe146bc3
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2025-03-07 23:52:13 +0600

    Exam1 Done

--- src/WebApplication11/WebApplication11/Program.cs
+++ src/WebApplication11/WebApplication11/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
+builder.WebHost.UseUrls("http://*:80");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
