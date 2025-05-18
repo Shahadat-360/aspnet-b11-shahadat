746eea36d42fcce9f20ddb2920df4810ce1282d5
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-05-18 22:48:14 +0600

    Add Infrastructure Layer of Clean Architecture Add Repository,Repostory ,Unit Of Work,SQLUtility Add TempDataExtensions For Notifications

---
+++ DevSkill.Inventory/DevSkill.Inventory.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevSkill.Inventory.Domain.Entities;
+using DevSkill.Inventory.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Infrastructure.Repositories
+{
+    public class ProductRepository(ApplicationDbContext dbContext): Repository<Product, Guid>(dbContext), IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext = dbContext;
+    }
+}
---
+++ DevSkill.Inventory/DevSkill.Inventory.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DevSkill.Inventory.Domain;
+using DevSkill.Inventory.Domain.Repositories;
+using DevSkill.Inventory.Domain.Utilities;
+using DevSkill.Inventory.Infrastructure.Repositories;
+using DevSkill.Inventory.Infrastructure.Utilities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Infrastructure
+{
+    public abstract class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+        protected ISqlUtility _sqlUtility { get; private set; }
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _sqlUtility = new SqlUtility(dbContext.Database.GetDbConnection());
+        }
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
