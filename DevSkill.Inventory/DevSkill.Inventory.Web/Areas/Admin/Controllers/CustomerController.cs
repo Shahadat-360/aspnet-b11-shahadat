bc7e2352bb6679a01e37a4896a21236d47694ddc
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-06-27 00:06:02 +0600

    - Active Menu Helper (Layout modified) - CustomerController (Index(Created Only))

---
+++ DevSkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
