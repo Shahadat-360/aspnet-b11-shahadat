5023f13e171cbaa61b6f6df5e8a1bba0953e61a7
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-06 22:32:34 +0600

    - Add Image Service Interface - Adding Images to Hard Drive - Delete Images From Hard Drive

---
+++ DevSkill.Inventory/DevSkill.Inventory.Application/Services/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Application.Services
+{
+    public interface IImageService
+    {
+        Task<string> SaveImageAsync(IFormFile ImageFile, string Folder);
+        Task<bool> DeleteImageAsync(string ImagePath);
+    }
+}
