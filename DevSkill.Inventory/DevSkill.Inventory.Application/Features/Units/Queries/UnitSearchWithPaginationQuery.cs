27d2fa1a0e5e9afb9239f3e3c8a1850f85bebd9b
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-06 22:30:41 +0600

    - Add Unit - Unit Search With Pagination For Product

---
+++ DevSkill.Inventory/DevSkill.Inventory.Application/Features/Units/Queries/UnitSearchWithPaginationQuery.cs
@@ -0,0 +1,19 @@
+﻿using DevSkill.Inventory.Domain.Dtos;
+using DevSkill.Inventory.Domain.Entities;
+using MediatR;
+using Unit = DevSkill.Inventory.Domain.Entities.Unit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Application.Features.Units.Queries
+{
+    public class UnitSearchWithPaginationQuery:IRequest<PaginatedResult<Unit>>
+    {
+        public string term=string.Empty;
+        public int page = 1; 
+        public int pageSize = 5;
+    }
+}
