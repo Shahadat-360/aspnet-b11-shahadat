1e4da21f0a8b947e5e4d084e0faefc95b6b8b080
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-06 22:51:51 +0600

    - Major Update In ProductsController - Add CategoriesController (Create,Search for Select2 Jquery) - Add UnitsController (Create,Search for Select2 Jquery)

---
+++ DevSkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,69 @@
+﻿using DevSkill.Inventory.Application.Features.Categories.Commands;
+using DevSkill.Inventory.Application.Features.Categories.Queries;
+using DevSkill.Inventory.Domain.Dtos;
+using DevSkill.Inventory.Domain.Entities;
+using DevSkill.Inventory.Domain.Enums;
+using DevSkill.Inventory.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController(IMediator mediator,ILogger<CategoriesController> logger) : Controller
+    {
+        private readonly IMediator _mediator = mediator;
+        private readonly ILogger<CategoriesController> _logger = logger;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchCategories(string term = "", int page = 1, int pageSize = 5)
+        {
+            try
+            {
+                var result = await _mediator.Send(new CategorySearchWithPaginationQuery { term = term, page = page, pageSize = pageSize });
+
+                var results = result.Items.Select(c => new
+                {
+                    id = c.Id.ToString(),
+                    text = c.Name,
+                    status = c.Status.ToString(),
+                    disabled = c.Status == Status.Inactive
+                }).ToList();
+
+                return Json(new
+                {
+                    results = results,
+                    pagination = new
+                    {
+                        more = result.HasNextPage
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error searching categories");
+                return Json(new { results = new List<object>(), pagination = new { more = false } });
+            }
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCategorySelect2([FromBody] CategoryAddCommand categoryAddCommand)
+        {
+            if (string.IsNullOrWhiteSpace(categoryAddCommand.Name))
+            {
+                return BadRequest("Name cannot be empty");
+            }
+            var newCategory = await _mediator.Send(categoryAddCommand);
+            return Json(new
+            {
+                Id = newCategory.Id.ToString(),
+                Name = newCategory.Name,
+            });
+        }
+    }
+}
---
+++ DevSkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/UnitsController.cs
@@ -0,0 +1,64 @@
+﻿using DevSkill.Inventory.Application.Features.Units.Commands;
+using DevSkill.Inventory.Application.Features.Units.Queries;
+using DevSkill.Inventory.Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UnitsController(IMediator mediator,ILogger<UnitsController> logger) : Controller
+    {
+        private readonly IMediator _mediator = mediator;
+        private readonly ILogger<UnitsController> _logger = logger;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> SearchUnits(string term = "", int page = 1, int pageSize = 5)
+        {
+            try
+            {
+                var result = await _mediator.Send(new UnitSearchWithPaginationQuery { term = term, page = page, pageSize = pageSize });
+
+                var results = result.Items.Select(c => new
+                {
+                    id = c.Id.ToString(),
+                    text = c.Name,
+                    status = c.Status.ToString(),
+                    disabled = c.Status == Status.Inactive
+                }).ToList();
+
+                return Json(new
+                {
+                    results = results,
+                    pagination = new
+                    {
+                        more = result.HasNextPage
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error searching categories");
+                return Json(new { results = new List<object>(), pagination = new { more = false } });
+            }
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateUnitSelect2([FromBody] UnitAddCommand unitAddCommand)
+        {
+            if (string.IsNullOrWhiteSpace(unitAddCommand.Name))
+            {
+                return BadRequest("Name cannot be empty");
+            }
+            var newCategory = await _mediator.Send(unitAddCommand);
+            return Json(new
+            {
+                Id = newCategory.Id.ToString(),
+                Name = newCategory.Name,
+            });
+        }
+    }
+}
