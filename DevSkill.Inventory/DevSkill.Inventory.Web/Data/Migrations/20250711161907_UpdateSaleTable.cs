dbb8c04f7f35af96f94c16302e2b68efdc6c2d08
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-13 23:55:05 +0600

    - Add Sale Payment Partial View - Update Migration File of Sale Entity - Logo Image Temporarily

---
+++ DevSkill.Inventory/DevSkill.Inventory.Web/Data/Migrations/20250711161907_UpdateSaleTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevSkill.Inventory.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSaleTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "Sales");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitPrice",
+                table: "Sales",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
