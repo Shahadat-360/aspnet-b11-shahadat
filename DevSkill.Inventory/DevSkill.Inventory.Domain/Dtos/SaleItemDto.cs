9ff5bc95a20da06aa31590fd842266cd51e7f2b1
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-13 23:11:50 +0600

    - Add SaleDto For ViewSale - Add SaleItemDto for Add and Update Sale - Add SaleSearchDto for Customized Column Search in DataTables - Add SalesIndexViewDto for View DataTables

---
+++ DevSkill.Inventory/DevSkill.Inventory.Domain/Dtos/SaleItemDto.cs
@@ -0,0 +1,18 @@
+﻿using DevSkill.Inventory.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Dtos
+{
+    public class SaleItemDto
+    {
+        public string? Id {  get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
---
+++ DevSkill.Inventory/DevSkill.Inventory.Domain/Dtos/SaleSearchDto.cs
@@ -0,0 +1,25 @@
+﻿using DevSkill.Inventory.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Dtos
+{
+    public class SaleSearchDto
+    {
+        public string? Id { get; set; }
+        public DateOnly? SaleDateFrom { get; set; }
+        public DateOnly? SaleDateTo { get; set; }
+        public string? CustomerName { get; set; }
+        public string? CustomerPhone { get; set; }
+        public decimal? MinTotal {  get; set; }
+        public decimal? MaxTotal { get; set; }
+        public decimal? MinPaid { get; set; }
+        public decimal? MaxPaid { get; set; }
+        public decimal? MinDue { get; set; }
+        public decimal? MaxDue { get; set; }
+        public PaymentStatus? PaymentStatus { get; set; }
+    }
+}
