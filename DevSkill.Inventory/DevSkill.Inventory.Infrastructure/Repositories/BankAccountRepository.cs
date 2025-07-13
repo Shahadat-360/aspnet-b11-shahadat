de5cb530bc808631ee962d83af44390910c309a1
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-13 23:45:29 +0600

    - Update ApplicationDbContext for Sale,BankAccount,CashAccount,MobileAccount and SaleItems - DataTables Stored Procedure Function Implementation,Assign Repositories - Add BankAccountRepository,CashAccountRepository,MobileAccountRepository and SaleRepository

---
+++ DevSkill.Inventory/DevSkill.Inventory.Infrastructure/Repositories/BankAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using DevSkill.Inventory.Domain.Dtos;
+using DevSkill.Inventory.Domain.Entities;
+using DevSkill.Inventory.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Infrastructure.Repositories
+{
+    public class BankAccountRepository(ApplicationDbContext applicationDbContext)
+        : Repository<BankAccount, int>(applicationDbContext), IBankAccountRepository
+    {
+        public async Task<PaginatedResult<BankAccount>> SearchBankAccounthWithPaginationAsync(string term, int page, int pageSize)
+        {
+            return await SearchWithPaginationAsync(
+                b=>b.AccountName.Contains(term),
+                q=>q.OrderBy(b=>b.Id),
+                page, pageSize);
+        }
+    }
+}
