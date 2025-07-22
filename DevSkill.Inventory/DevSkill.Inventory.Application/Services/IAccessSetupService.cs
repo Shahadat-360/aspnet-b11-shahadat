900e14f9135041f5b78a9914029dfa294b02cb53
MD. Shahadat Hossain Bhuian <1337.shahadat@gmail.com>
2025-07-22 17:49:26 +0600

    - Add Services for Access Setup,Role Service,Transaction Service and User Service Interface

---
+++ DevSkill.Inventory/DevSkill.Inventory.Application/Services/IAccessSetupService.cs
@@ -0,0 +1,16 @@
+﻿using DevSkill.Inventory.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Application.Services
+{
+    public interface IAccessSetupService
+    {
+        Task<List<AccessSetupIndexDto>> GetRolesAsync();
+        Task<List<RoleClaimsDto>> GetRoleClaimsAsync(Guid roleId);
+        Task<bool> UpdateRoleClaimsAsync(string roleId, List<string> selectedClaims);
+    }
+}
